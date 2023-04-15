2db13159ffa085825da8c507faabee49984c06b2
Kevin Alexis Arias <83034131+Kimiwax@users.noreply.github.com>
2023-04-15 07:12:29 -0300

    enviroment vars

--- index.ts
+++ index.ts
@@ -2,9 +2,16 @@ import * as express from "express";
 
 const app = express();
 const PORT = 3000;
+
 app.get("/hola", (req, res) => {
   res.json({
     message: "Hola desde el servidor",
+  });
+});
+
+app.get("/env", (req, res) => {
+  res.json({
+    enviroment: process.env.ENVIROMENT,
   });
 });
 
